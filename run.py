e60e4d1a744baab1f7de6d32f96da01caf65ceb1
Mikatep <miriamcordobacapote@gmail.com>
2022-06-01 15:06:21 +0000

    problem with about page sorted

--- run.py
+++ run.py
@@ -13,12 +13,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -31,11 +33,10 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-
-@app.route("/contact", methods=["GET","POST"])
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have received your message!" .format(
+        flash("Thanks {}, we have received your message!".format(
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
@@ -50,4 +51,3 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-    
